50dff87c981ab7b204631b46a584ccd161555fc0
RogerioDP44 <115008024+RogerioDP44@users.noreply.github.com>
2026-02-17 20:23:48 -0300

    Add files via upload

--- api/criarPreferencia.js
+++ api/criarPreferencia.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
 
   const { idUsuario, plano } = req.body;
-  const valor = plano === 'anual' ? 199.90 : 29.90;
+  const valor = plano === 'anual' ? 149.90 : 0.90;
 
   try {
     const response = await fetch("https://api.mercadopago.com/checkout/preferences", {
